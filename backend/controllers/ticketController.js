3679571074020d0bee3995d625ef1d5112df8bbe
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-14 14:35:20 -0400

    add route and controller function to get a single ticket, by passing ticket ID as URL param to endpt and validating userID matches the creator's uid

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -17,9 +17,38 @@ const getTickets = asyncHandler(async (req, res) => {
         throw new Error('User not found');
     }
 
+    // finds ALL tickets created by this user
     const tickets = await Ticket.find({ user: req.user.id });
 
     res.status(200).json(tickets);
+});
+
+// @desc get user ticket
+// @route /api/tickets/me
+// @access Private
+const getTicket = asyncHandler(async (req, res) => {
+    // Get user based on _id in webToken
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        res.status(401);
+        throw new Error('User not found');
+    }
+
+    // Finds SINGLE ticket with ID matching one given as URL param ( /:id ) (see ticketRoutes)
+    const ticket = await Ticket.findById(req.params.id);
+
+    if (!ticket) {
+        res.status(404);
+        throw new Error('Ticket not found');
+    }
+
+    if (ticket.user.toString() !== req.user.id) {
+        res.status(401);
+        throw new Error('User not authorized to access this ticket');
+    }
+
+    res.status(200).json(ticket);
 });
 
 // @desc create new ticket
@@ -51,4 +80,4 @@ const createTicket = asyncHandler(async (req, res) => {
     res.status(201).json(ticket);
 });
 
-module.exports = { getTickets, createTicket };
+module.exports = { getTickets, getTicket, createTicket };
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const { getTickets, createTicket } = require('../controllers/ticketController');
+const {
+    getTickets,
+    getTicket,
+    createTicket,
+} = require('../controllers/ticketController');
 
 const router = express.Router();
 
@@ -7,5 +11,5 @@ const { protect } = require('../middleware/authMiddleware');
 
 // GET / POST ticket routes, protected (logged in users only)
 router.route('/').get(protect, getTickets).post(protect, createTicket);
-
+router.route('/:id').get(protect, getTicket);
 module.exports = router;
