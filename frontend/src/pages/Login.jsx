d1ca6a6b1ba7f03632609752ce0ea251c6f34ed4
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-14 13:05:18 -0400

    able to retain user data after registering to keep user logged in. Logout destroys user token. TODO: login - vaerify user with DB, and give token

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { FaSignInAlt } from 'react-icons/fa';
 import { toast } from 'react-toastify';
+import { useSelector, useDispatch } from 'react-redux';
+import { login } from '../features/auth/authSlice';
 
 function Login() {
     const [formData, setFormData] = useState({
@@ -12,6 +14,14 @@ function Login() {
 
     const { email, password } = formData;
 
+    // Initialize dispatch for redux
+    const dispatch = useDispatch();
+
+    // import state info needed
+    const { user, isLoading, isSuccess, message } = useSelector(
+        (state) => state.auth
+    );
+
     const onChange = (e) => {
         setFormData((prevState) => ({
             ...prevState,
@@ -20,7 +30,13 @@ function Login() {
     };
 
     const onSubmit = (e) => {
-        console.log(e.target.value);
+        e.preventDefault();
+
+        const userData = {
+            email,
+            password,
+        };
+        dispatch(login(userData));
     };
 
     return (
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FaUser } from 'react-icons/fa';
 import { toast } from 'react-toastify';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { register, reset } from '../features/auth/authSlice';
 
 function Register() {
     const [formData, setFormData] = useState({
@@ -11,6 +14,26 @@ function Register() {
     });
 
     const { name, email, password, password2 } = formData;
+
+    const dispatch = useDispatch();
+
+    const navigate = useNavigate();
+
+    const { user, isLoading, isError, isSuccess, message } = useSelector(
+        (state) => state.auth
+    );
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
+        // Redirect to Home when logged in w/ Navigate
+        if (isSuccess || user) {
+            navigate('/');
+        }
+
+        dispatch(reset());
+    });
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -25,6 +48,14 @@ function Register() {
 
         if (password !== password2) {
             toast.error('Passwords do not match!');
+        } else {
+            const userData = {
+                name,
+                email,
+                password,
+            };
+
+            dispatch(register(userData));
         }
     };
 
