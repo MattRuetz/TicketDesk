a080ba2c605c4438eaa1fe47700ccf01b5dfbce8
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-14 14:24:11 -0400

    comment to ticketToutes

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 const { protect } = require('../middleware/authMiddleware');
 
+// GET / POST ticket routes, protected (logged in users only)
 router.route('/').get(protect, getTickets).post(protect, createTicket);
 
 module.exports = router;
