a2c779f82cb274c71ea2ecb354708399c48e07a6
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-15 13:17:20 -0400

    connect ticket reducer to backend. tickets get stored in DB with user auth info to be retirieved by the creator

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -10,6 +10,30 @@ const initialState = {
     message: '',
 };
 
+// Create new Ticket
+export const createTicket = createAsyncThunk(
+    'tickets/create',
+    async (ticketData, thunkAPI) => {
+        try {
+            // This is why redux toolkit is nice ... can get from another state with getState
+            const token = thunkAPI.getState().auth.user.token;
+            // state.pending... -> state.fulfilled (async)
+            console.log(ticketData);
+            return await ticketService.createTicket(ticketData, token);
+        } catch (error) {
+            // state.rejected
+            const message =
+                (error.response &&
+                    error.response.data &&
+                    error.response.data.message) ||
+                error.message ||
+                error.toString();
+
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+);
+
 export const ticketSlice = createSlice({
     name: 'ticket',
     initialState,
@@ -17,7 +41,22 @@ export const ticketSlice = createSlice({
         // (actions)
         reset: (state) => initialState,
     },
-    extraReducers: (builder) => {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(createTicket.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(createTicket.fulfilled, (state) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+            })
+            .addCase(createTicket.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.message = action.payload;
+                console.log('rejected');
+            });
+    },
 });
 
 export const { reset } = ticketSlice.actions;
