9be07607a96b7919e80db5fad345fb7afb75aa9f
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-16 11:14:00 -0400

    add script to serve frontend when process is in production mode, and point to static files in build folder for paths to/from frontend. add heroku-postbuild CLI command in root package.json to install and build the frontend app and dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
@@ -15,13 +16,25 @@ app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to the Support Desk API' });
-});
-
 // Can create endpoints from other files by adding as middleware with app.use
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+// Serve frontent
+if (process.env.NODE_ENV === 'production') {
+    // point express to static (build) folder
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    // load index.html to serve
+    app.get('*', (req, res) =>
+        res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+    );
+} else {
+    // serve backend API
+    app.get('/', (req, res) => {
+        res.status(200).json({ message: 'Welcome to the Support Desk API' });
+    });
+}
+
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
